7e9c5f870ba776d627cb6226c6f4bffc5ddfde23
Ash Pook <sungiant@live.co.uk>
2018-09-14 23:53:46 +0100

    Better.

--- docs.sh
+++ docs.sh
@@ -4,9 +4,12 @@ set -e
 set -x
 
 echo 'Setting up the docs script...'
-ROOT_DIR=$(pwd)
-mkdir docs
-cd docs
+
+doxygen doxygen.conf
+
+mkdir temp/
+cd temp/
+
 git clone -b gh-pages https://git@$GH_REPO_REF
 cd $GH_REPO_NAME
 git config --global push.default simple
@@ -14,10 +17,9 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 rm -rf *
 echo "" > .nojekyll
-echo 'Generating Doxygen code documentation...'
-doxygen ${ROOT_DIR}/doxygen.conf 2>&1 | tee doxygen.log
+mv ../docs/html/ .
 
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+if [ -f "index.html" ]; then
     echo 'Uploading documentation to the gh-pages branch...'
     git add --all
     git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
